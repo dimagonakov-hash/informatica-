3acb9899a1f1a1a62092d1415058e82ee84a3c53
MacWyser <dimagonakov@gmail.com>
2025-10-27 14:13:19 +0100

    nic

---
+++ 1program/koziawki 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ int main(){
+ 
+   int a = 8;
+   int b = 3;
+int suma = a + b;
+    int roznica = a - b;
+    int iloczyn = a * b;
+    
+    cout << "Liczba a = " << a<< endl;
+    cout << "Liczba b = " << b<< endl;
+    cout << "Suma: " << suma;
+    cout << "Roznica: " << roznica<< endl;
+    cout << "Iloczyn: " << iloczyn<< endl;
+    
+    cout << 8 << a;
+    cout << 3 << b;
+return 0;
+}
