e985e49bb39cb411f4053161ce0de93a54c32519
MacWyser <dimagonakov@gmail.com>
2025-11-17 13:07:29 +0100

    nic

---
+++ 1program/8=3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+    int n;{
+
+    std::cout << "?????????: 8" << equals << "3\n";
+    std::cout << " ??????? '=': ";
+    std::cin >> n;
+
+   
+    equals.append(n, '=');
+
+    std::cout << "??????: 8" << equals << "3\n";
+
+    return 0;
+}
