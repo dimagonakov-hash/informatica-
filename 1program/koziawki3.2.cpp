2ae57c58ef33a96f408e7f2dcdc339a6c0912dd2
MacWyser <dimagonakov@gmail.com>
2025-11-24 14:07:31 +0100

    3,1 3,2 3,3

---
+++ 1program/koziawki3.2.cpp
@@ -0,0 +1,18 @@
+#include using namespace std;
+
+int main() {
+int N;
+
+cout << "Podaj liczbe N: ";
+cin >> N;
+int suma = 0;
+int i =1;
+
+while(i <= N)
+{
+suma += i;
+i++;
+}
+cout << "suma liczb naturalnych:" << suma << endl;
+
+return 0;}
---
+++ 1program/koziawki3.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+int number;
+
+cout << "Podaj liczbe: ";
+cin >> number;
+ 
+cout << "Wprowadzilesz liczbe" << number <<endl;
+cout << "Czy chcesz wprowadz keleinom liczbe(t/n)";
+	
+	char wybor;
+	cin >> wybor;
+    
+	
+	
+	
+return 0;}
